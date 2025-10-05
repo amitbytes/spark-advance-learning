6f188d4c79174aac5fe524b3e87c163c066f8c66
Amit Singh <elysium@outlook.in>
2025-10-05 19:22:48 +0530

    Spark configuration commands added into comments

--- src/main/scala/org/amitbytes/common/SparkConfigLoader.scala
+++ src/main/scala/org/amitbytes/common/SparkConfigLoader.scala
@@ -13,3 +13,18 @@ object SparkConfigLoader {
     sparkConf
   }
 }
+
+/*
+	spark.sql("SET -v").show()
+	spark.sql("SET -v").where($"key".like("%spark.sql.adaptive%")).show(100,false)
+	--Colaesceing broadscast join
+	spark.sql.adaptive.autoBroadcastJoinThreshHold=10MB default we can set it -1 negate this feature
+    spark.sql.adaptive.localShuffleReader.enabled=true  -- keep it true always if we are using broadcast join AEQ feature
+
+	sc.parallelize(spark.sparkContext.getConf.getAll).toDF("key","value").where($"key".like("%spark.sql%")).show(false)
+	sc.parallelize(spark.conf.getAll.toSeq).toDF("key","value").where($"key".like("%spark.sql%")).show(false)
+
+	spark.sql("SET -v").where($"key".like("spark.sql.adaptive%")).select("key","value","since version").show(100,false)
+	spark.sql("SET -v").where($"key".like("spark.sql.shuffle%")).select("key","value","since version").show(100,false)
+	spark.sql("SET -v").where($"key".like("spark.sql.files%")).select("key","value","since version").show(100,false)
+ */
