f7cbc96b2cb390ec01a38ab3059b76e16fab3d0b
Amit Singh <elysium@outlook.in>
2025-11-10 11:05:06 +0530

    word frequency count program using dataframe

--- src/main/scala/org/amitbytes/etl/App.scala
+++ src/main/scala/org/amitbytes/etl/App.scala
@@ -2,7 +2,7 @@ package org.amitbytes.etl
 import org.apache.spark.sql.{SaveMode, SparkSession, functions => F}
 import org.amitbytes.common.{CommonHelper, DatabasesEnum, SparkConfigLoader, SparkSessionFactory}
 import org.apache.spark.sql.expressions.Window
-import org.amitbytes.transformtions.{BaseTransformation, TransformationFactory}
+import org.amitbytes.transformtions.{BaseTransformation, FindWordCountFreQuencyTransformation, TransformationFactory}
 import org.slf4j.LoggerFactory
 import org.amitbytes.exention.methods.{AllExtensionMethdods, DataFrameImplicits, SparkSessionExtensions}
 import org.amitbytes.filequestions.{DataCleanAnalysis, NestedJsonExample, SalesDataAnalysis, ScalaLearning, WordFrequencyCount}
@@ -24,12 +24,20 @@ object App extends SparkSessionFactory {
       spark.printSparkVersion()
       import spark.implicits._
 
-      val schema ="employee_id STRING,department_id STRING,name STRING,age STRING,gender STRING,salary STRING,hire_date STRING"
-      var emp_df = spark.read.format("csv").schema(schema).option("header","true").load("input/csvs/emp.csv")
-      emp_df = emp_df.withColumn("name_gender",F.concat_ws(" ",$"name",$"gender"))
-      emp_df.displayData();
+      //val stream_df = spark.readStream.format("csv").option("header","true").schema("").load();
+      //val query = stream_df.writeStream.format("delta").trigger(availableNow=true).outputMode("append").option("checkpointLocation","").queryName("daadjlsf").toTable("database.table")
+      //query.awaitTermination() //will wait until data processing is completed
+      //val schema ="employee_id STRING,department_id STRING,name STRING,age STRING,gender STRING,salary STRING,hire_date STRING"
+      //var emp_df = spark.read.format("csv").schema(schema).option("header","true").load("input/csvs/emp.csv")
+      //emp_df = emp_df.withColumn("name_gender",F.concat_ws(" ",$"name",$"gender"))
+      //emp_df.displayData();
 
-      logger.info("Transformation completed")
+      //logger.info("Transformation completed")
+
+      val wordsDF = spark.read.text("input/txtfiles/words.txt")
+      val wordFrequencyCount = new FindWordCountFreQuencyTransformation()
+      val returnDF = wordFrequencyCount.runTransformation(wordsDF)
+      returnDF.show(false)
       /*
       * --class org.amitbytes.etl.App --jars s3://emr-serverless-learning-studio/jarfiles/mysql-connector-j-8.0.33.jar,s3://emr-serverless-learning-studio/jarfiles/HikariCP-7.0.2.jar,s3://emr-serverless-learning-studio/jarfiles/config-1.4.3.jar --files s3://emr-serverless-learning-studio/appfiles/application.conf,s3://emr-serverless-learning-studio/appfiles/spark.conf --conf spark.hadoop.hive.metastore.client.factory.class=com.amazonaws.glue.catalog.metastore.AWSGlueDataCatalogHiveClientFactory
       * */
@@ -37,11 +45,12 @@ object App extends SparkSessionFactory {
       case e: Exception =>
         println(e.getMessage)
         logger.error("Error occurred: ", e)
+
         System.exit(1)
     }
     finally {
-      //scala.io.StdIn.readLine("Hit enter to exit")
-      spark.stop()
+      scala.io.StdIn.readLine("Hit enter to exit")
+      //spark.stop()
     }
   }
 }
--- src/main/scala/org/amitbytes/transformtions/FindWordCountFreQuencyTransformation.scala
+++ src/main/scala/org/amitbytes/transformtions/FindWordCountFreQuencyTransformation.scala
@@ -1,5 +1,22 @@
 package org.amitbytes.transformtions
+import org.apache.spark.sql.{DataFrame, SparkSession}
 
-class FindWordCountFreQuencyTransformation {
 
+class FindWordCountFreQuencyTransformation extends BaseTransformation {
+
+  /**
+   * This method will be implemented by all the transformation classes
+   *
+   * @param df    Input DataFrame
+   * @param spark Implicit SparkSession
+   * @return Transformed DataFrame
+   */
+  override def runTransformation(df: DataFrame)(implicit spark: SparkSession): DataFrame = {
+    import spark.implicits._
+    val wordsDF = df.selectExpr("explode(split(value, ' ')) as word")
+    val wordCountDF = wordsDF.selectExpr("lower(word) as word").groupBy("word").count().withColumnRenamed("count", "frequency")
+    wordCountDF.orderBy($"frequency".desc)
+  }
+
+  override val transformationName: String = "FindWordCountFreQuencyTransformation"
 }
